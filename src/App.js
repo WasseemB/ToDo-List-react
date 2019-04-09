fc4bcd754f7700367ee5219ced5fd7bf78a98d7c
WasseemB <the.waswas@gmail.com>
2019-04-09 13:00:32 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,87 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Todos from './components/Todos';
+import Header from './components/layout/Header';
+import AddToDo from './components/AddToDo';
+import About from './components/pages/About';
+import axios from 'axios';
+//import uuid from 'uuid';
+
 import './App.css';
 
 class App extends Component {
+  state = {
+    todos: []
+  };
+
+  async componentDidMount() {
+    const res = await axios.get(
+      'https://jsonplaceholder.typicode.com/todos?_limit=10'
+    );
+    this.setState({ todos: res.data });
+  }
+  //delete todo
+  delTodo = async id => {
+    const res = await axios.delete(
+      `https://jsonplaceholder.typicode.com/todos/${id}`
+    );
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.title = 'deleted';
+        }
+        return todo;
+      })
+    });
+  };
+  //addTodo
+
+  addTodo = async title => {
+    const res = await axios.post('https://jsonplaceholder.typicode.com/todos', {
+      title,
+      completed: false
+    });
+    this.setState({
+      todos: [...this.state.todos, res.data]
+    });
+  };
+  //toggle complete
+  markComplete = id => {
+    //console.log(id);
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      })
+    });
+  };
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div className="App">
+          <div className="container">
+            <Header />
+            <Route
+              path="/"
+              exact
+              render={props => (
+                <React.Fragment>
+                  <AddToDo addTodo={this.addTodo} />
+                  <Todos
+                    todos={this.state.todos}
+                    markComplete={this.markComplete}
+                    delTodo={this.delTodo}
+                  />
+                </React.Fragment>
+              )}
+            />
+            <Route path="/about" component={About} />
+          </div>
+        </div>
+      </Router>
     );
   }
 }
